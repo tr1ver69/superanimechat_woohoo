c9df4a04f44f512f9b3d844e148582dada05f3e6
elnijs <Elias.Nijs@UGent.be>
2022-05-21 13:00:37 +0200

    added improts

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import './styles/styles.css';
+
+
+ReactDOM.render(<p>Hallo</p>, document.getElementById('root'));
